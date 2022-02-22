e36d60efe27d463be06e93097da01b05f1bd1abf
lucaslevii <lucas.levi@useargo.com>
2022-02-22 16:32:38 -0300

    Spring setup

---
+++ src/test/java/com/voting/ruling/RulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting.ruling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
