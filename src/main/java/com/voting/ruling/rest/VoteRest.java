2329eb8e935818e657b82a5a5b8e3ba6be4a608f
lucaslevii <lucas.levi@useargo.com>
2022-02-25 07:46:30 -0300

    Vote structure

--- src/main/java/com/voting/ruling/rest/VoteRest.java
+++ src/main/java/com/voting/ruling/rest/VoteRest.java
@@ -1,4 +1,33 @@
 package com.voting.ruling.rest;
 
+
+import com.voting.ruling.service.AssociateService;
+import com.voting.ruling.service.RulingService;
+import com.voting.ruling.service.VoteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/vote")
 public class VoteRest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(VoteService.class);
+    @Autowired
+    RulingService rulingService;
+    @Autowired
+    VoteService voteService;
+    @Autowired
+    AssociateService associateService;
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.POST)
+    public void rulingVote(
+            @PathVariable Long id,
+            String cpf
+    ) {
+
+    }
 }
--- src/main/java/com/voting/ruling/service/VoteService.java
+++ src/main/java/com/voting/ruling/service/VoteService.java
@@ -1,12 +1,31 @@
 package com.voting.ruling.service;
 
+import com.voting.ruling.exception.BadRequestException;
+import com.voting.ruling.model.Vote;
 import com.voting.ruling.repository.VoteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VoteService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(VoteService.class);
 
     @Autowired
     private VoteRepository voteRepository;
+
+    public void vote(Vote vote) {
+        try {
+            LOGGER.debug("Computing vote...");
+            voteRepository.save(vote);
+            LOGGER.debug("The vote has been submitted");
+        } catch (Exception e) {
+            throw new BadRequestException(String.format("We can't compute the vote with id %d %s", vote.getId(), e.getMessage()));
+        }
+    }
+
+    public Integer countBySessionIdAndAssociateId(Long sessionId, Long rulingId) {
+        return voteRepository.countBySessionIdAndAssociateId(sessionId, rulingId);
+    }
 }
