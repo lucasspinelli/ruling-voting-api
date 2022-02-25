183d00ac1f55d5eb440ef45de76dcbd173f0444b
lucaslevii <lucas.levi@useargo.com>
2022-02-25 07:43:05 -0300

    refactor with debug log

--- src/main/java/com/voting/ruling/rest/AssociateRest.java
+++ src/main/java/com/voting/ruling/rest/AssociateRest.java
@@ -1,12 +1,12 @@
 package com.voting.ruling.rest;
 
+import com.voting.ruling.exception.BadRequestException;
 import com.voting.ruling.form.AssociateForm;
 import com.voting.ruling.model.Associate;
 import com.voting.ruling.service.AssociateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -22,32 +22,30 @@ public class AssociateRest {
     private AssociateService associateService;
 
     @RequestMapping(path = "/list", method = RequestMethod.GET)
-    public ResponseEntity associateList(){
-       return ResponseEntity.ok(associateService.associateList());
+    public ResponseEntity associateList() {
+        return ResponseEntity.ok(associateService.associateList());
     }
 
     @RequestMapping(path = "/register", method = RequestMethod.POST)
     public ResponseEntity register(@Valid @RequestBody AssociateForm associateForm,
-                                   UriComponentsBuilder uriComponentsBuilder){
+                                   UriComponentsBuilder uriComponentsBuilder) {
         Associate associate = associateForm.toAssociate();
-        try{
+        try {
             associateService.save(associate);
             URI uri = uriComponentsBuilder.path("associate/{id}").buildAndExpand(associate.getId()).toUri();
             return ResponseEntity.created(uri).build();
         } catch (Exception e) {
-            logger.error("Cannot save associate", e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            throw new BadRequestException("Cannot save associate " + e.getMessage());
         }
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity getAssociateById(@PathVariable(name = "id") Long id){
-        try{
+    public ResponseEntity getAssociateById(@PathVariable(name = "id") Long id) {
+        try {
             Associate associate = associateService.getById(id);
             return ResponseEntity.ok(associate);
-        } catch (Exception e){
-            logger.error(String.format("Cannot find associate with id %d %s", id, e.getMessage()));
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Associate not Founded " + e.getMessage());
+        } catch (Exception e) {
+            throw new BadRequestException(String.format("Cannot find associate with id %d %s", id, e.getMessage()));
         }
     }
 }
--- src/main/java/com/voting/ruling/service/RulingService.java
+++ src/main/java/com/voting/ruling/service/RulingService.java
@@ -1,7 +1,10 @@
 package com.voting.ruling.service;
 
+import com.voting.ruling.exception.NotFoundException;
 import com.voting.ruling.model.Ruling;
 import com.voting.ruling.repository.RulingRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,18 +12,26 @@ import java.util.List;
 
 @Service
 public class RulingService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Ruling.class);
+
     @Autowired
     private RulingRepository rulingRepository;
 
-    public void save(Ruling ruling){
+    public void save(Ruling ruling) {
         rulingRepository.save(ruling);
     }
 
-    public List<Ruling> rulingList(){
+    public List<Ruling> rulingList() {
         return rulingRepository.findAll();
     }
 
     public Ruling getById(Long id) {
-        return rulingRepository.findById(id).get();
+        try {
+            LOGGER.debug("Trying to find ruling with id " + id);
+            return rulingRepository.findById(id).get();
+        } catch (Exception e) {
+            throw new NotFoundException(String.format("Cannot find ruling with id %d %s", id, e.getMessage()));
+        }
+
     }
 }
